87673550a15794001f42e3671c8a9bf9aadfc541
Deivysson Gomes <105027772+Deivysson@users.noreply.github.com>
2024-01-08 21:15:59 -0300

    segundo commit
    configurando as rotas e instalando o Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
 
-function App() {
+import { Home } from "./pages/home";
+import { Login } from "./pages/login";
+import { Admin } from "./pages/admin";
+import { Networks } from "./pages/networks";
 
-  return (
-      <div>
-        <h1>Teste</h1>
-      </div>
-      
-  )
-}
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <Home />
+  },
+  {
+    path:'/login',
+    element: <Login />
+  },
+  {
+    path:'/admin/social',
+    element: <Networks />
+  },
+  {
+    path:'/admin',
+    element: <Admin />
+  }
 
-export default App
+])
+
+export { router };
